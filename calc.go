afe2520bf544c51d550347bd8f7de1177c983b2f
Vdmrrr <matphyx@gmail.com>
2024-07-29 13:01:31 +0300

    add latin numc I - C, add arabic nums 1-100

--- calc.go
+++ calc.go
@@ -10,8 +10,28 @@ import (
 	"strings"
 )
 
-var latinNums = [9]string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
-var arabicNums = [9]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+var latinNums = [100]string{
+	"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+	"XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+	"XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+	"XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+	"XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+	"LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+	"LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+	"LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+	"LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+	"XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C",
+}
+
+var arabicNums = CreateNumbersSlice()
+
+func CreateNumbersSlice() []string {
+	numbers := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		numbers[i] = strconv.Itoa(i + 1)
+	}
+	return numbers
+}
 
 func DeleteEmptySlice(s []string) []string {
 	var r []string
@@ -87,7 +107,7 @@ func CheckText(text string) error {
 		return err
 	}
 
-	regularCheck := regexp.MustCompile(`^(?:[1-9]|I|II|III|IV|V|VI|VII|VIII|IX)[+\-*/](?:[1-9]|I|II|III|IV|V|VI|VII|VIII|IX)$`)
+	regularCheck := regexp.MustCompile(`^(?:[1-9]|10|I|II|III|IV|V|VI|VII|VIII|IX|X)[+\-*/](?:[1-9]|10|I|II|III|IV|V|VI|VII|VIII|IX|X)$`)
 	if !(regularCheck.MatchString(text)) {
 		err := errors.New("Выдача паники, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
 		return err
